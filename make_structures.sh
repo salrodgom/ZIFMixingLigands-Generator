6b793b2ff2b1d5014a934cc3785db3f0b0af7736
Salvador Rodriguez Gomez <salrodgom@upo.es>
2018-06-23 20:43:54 +0200

    modified:   make_structures.sh 	modified:   src/Makefile

--- make_structures.sh
+++ make_structures.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 make
 for topology in SOD SZ7 ; do
- #for linker_1 in imi mim bim; do
- #  ./generator -t $topology -l 1 ${linker_1} 1.0 > ${topology}_${linker_1}_pure.txt
- #  cp ${topology}_${linker_1}_mixture.cif ${topology}_${linker_1}_pure.cif
- #done
- for linker_1 in mim ; do
-  for linker_2 in bim ; do
-   for molar_fraction in $(seq 0.10 0.10 0.90 ) ; do
+ for linker_1 in imi mim bim; do
+   ./generator -t $topology -l 1 ${linker_1} 1.0 > ${topology}_${linker_1}_pure.txt
+   cp ${topology}_${linker_1}_mixture.cif ${topology}_${linker_1}_pure.cif
+ done
+ for linker_1 in imi mim ; do
+  for linker_2 in mim bim ; do
+   if [ "${linker_1}" != "${linker_2}" ] ; then
+    for molar_fraction in $(seq 0.10 0.10 0.90 ) ; do
     ./generator -t $topology -l 2 ${linker_1} ${molar_fraction} ${linker_2} $(bc <<<"scale=2;1.0-${molar_fraction}") > ${topology}_${linker_1}_${molar_fraction}_${linker_2}_$(bc <<<"scale=2;1.0-${molar_fraction}")_mixture.txt
-    mv ${topology}_${linker_1}${linker_2}_mixture.cif ${topology}_${linker_1}_${molar_fraction}_${linker_2}_$(bc <<<"scale=2;1.0-${molar_fraction}")_mixture.cif
-   done
+     mv ${topology}_${linker_1}${linker_2}_mixture.cif ${topology}_${linker_1}_${molar_fraction}_${linker_2}_$(bc <<<"scale=2;1.0-${molar_fraction}")_mixture.cif
+    done
+   fi
   done
  done
 done
